652dd2c6ff9aeb206c969670d40daaf7227b1612
padhuka <it.hondasolobaru@gmail.com>
2025-10-09 15:17:30 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-
+  integrations: [vue(), sitemap()],
+  site: 'https://devsonlab.web.id', // Ganti dengan domain aslimu (WAJIB)
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+})
